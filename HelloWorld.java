d0f2d9c9b9c81b7fda4405e8f43810d964274045
ShinYoonHa <idc06012@naver.com>
2023-03-31 14:05:31 +0900

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-	public static void main(String[] args) {
-		String str = "Let me introduce myself~";
-		String newStr = str+"hello";
-		System.out.println("Hello World!!");
-		System.out.println(newStr);
-	}
-}
