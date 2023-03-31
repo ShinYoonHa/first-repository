65b02cb54cc453c02f6be1203348007e902db5a8
ShinYoonHa <idc06012@naver.com>
2023-03-31 14:09:39 +0900

    Two files are commited.

---
+++ ComAppConfig.java
@@ -0,0 +1,16 @@
+package com.kit.dormitory;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
+                classes = Configuration.class)
+)
+public class ComAppConfig {
+
+}
