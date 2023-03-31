29f2167bf9ec4c98bc442d743e7b7b70d38102e7
ShinYoonHa <idc06012@naver.com>
2023-03-31 14:00:26 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		String str = "Nice meet you!";
+		String str = "Let me introduce myself~";
 		System.out.println("Hello World!!");
 		System.out.println(str);
 		
